5a46c87108f90217fa8a474c699066df512e47ab
lfernc01 <60119443+lfernc01@users.noreply.github.com>
2020-06-10 01:22:42 +0200

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -33,3 +33,35 @@ public class HarshBoundedQueue {
 	}
 
 }
+
+public class NiceBoundedQueue {
+	private LinkedList<Integer> a = new LinkedList<>();
+	private int capacity = 10;
+	
+	public NiceBoundedIntQueue(int c) {
+		capacity = c;
+	}
+	
+	public boolean enqueue(int value) {
+		if(a.size() == capacity)
+			return false;
+		a.add(value);
+		return true;
+	}
+	
+	public boolean isEmpty() {
+		return a.isEmpty();
+	}
+	
+	public int dequeu() {
+		if(isEmpty())
+			return Integer.MIN_VALUE;
+		return a.removeFirst();
+	}
+	
+	public String toString() {
+		return Arrays.deepToString(a.toArray());
+	}
+
+
+}
